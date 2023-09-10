9af892e401fb99c3d7a161257e118f97fde24443
Matheus Miguel <matheusmiguelsa03@gmail.com>
2023-09-10 18:14:22 -0300

    2nd commit

---
+++ sfg-pet-clinic/src/main/java/guru/springframework/model/Pet.java
@@ -0,0 +1,85 @@
+package guru.springframework.model;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+public class Pet {
+    private PetType petType;
+    private Owner owner;
+    private LocalDate birthDate;
+
+    public Pet() {
+    }
+
+    public Pet(PetType petType, Owner owner, LocalDate birthDate) {
+        this.petType = petType;
+        this.owner = owner;
+        this.birthDate = birthDate;
+    }
+
+    public PetType getPetType() {
+        return this.petType;
+    }
+
+    public void setPetType(PetType petType) {
+        this.petType = petType;
+    }
+
+    public Owner getOwner() {
+        return this.owner;
+    }
+
+    public void setOwner(Owner owner) {
+        this.owner = owner;
+    }
+
+    public LocalDate getBirthDate() {
+        return this.birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public Pet petType(PetType petType) {
+        setPetType(petType);
+        return this;
+    }
+
+    public Pet owner(Owner owner) {
+        setOwner(owner);
+        return this;
+    }
+
+    public Pet birthDate(LocalDate birthDate) {
+        setBirthDate(birthDate);
+        return this;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this)
+            return true;
+        if (!(o instanceof Pet)) {
+            return false;
+        }
+        Pet pet = (Pet) o;
+        return Objects.equals(petType, pet.petType) && Objects.equals(owner, pet.owner) && Objects.equals(birthDate, pet.birthDate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(petType, owner, birthDate);
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+            " petType='" + getPetType() + "'" +
+            ", owner='" + getOwner() + "'" +
+            ", birthDate='" + getBirthDate() + "'" +
+            "}";
+    }
+    
+    
+}
